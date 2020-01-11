bf6a6338a11e1e9deb04ed4e662ccff6a1640887
704priyanka <32764617+704priyanka@users.noreply.github.com>
2020-01-11 23:54:50 +0530

    Update wcity.py

--- wcity.py
+++ wcity.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
-def city():
+def index():
         if request.method == 'GET':
-                city = request.args.get('city')
-        if city == None or city == '':
-                city = 'Mumbai'
+                index = request.args.get('index')
+        if index == None or index == '':
+                index = 'Mumbai'
 
-        req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+ city +'&units=metric&appid=271d1234d3f497eed5b1d80a07b3fcd1').json()
+        req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+ index +'&units=metric&appid=271d1234d3f497eed5b1d80a07b3fcd1').json()
         print(req)
 
         description = req['weather'][0]['description'].upper()
@@ -19,6 +19,6 @@ def city():
         icon = req['weather'][0]['icon']
         icon = 'https://openweathermap.org/img/w/'+ icon +'.png'
 
-        return render_template('index.html', city = city, description = description, temperature = temperature, icon = icon)
+        return render_template('index.html', index = index, description = description, temperature = temperature, icon = icon)
 if __name__ == '__main__':
    app.run()
